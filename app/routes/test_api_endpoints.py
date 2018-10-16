192d294de90d62385e9e0ff1325809b502bc2d55
muhwezideo <fahdjamy2@gmail.com>
2018-10-16 12:53:53 +0300

    [Deliver #161214653] add tests for create product api endpoint

---
+++ app/routes/test_api_endpoints.py
@@ -0,0 +1,20 @@
+import json
+from unittest import TestCase
+from app import app
+from app.models.products import Products
+
+
+class TestApiRoutesCase(TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.pdts_db = Products()
+        self.pdts_db.delete_products()
+
+    def test_api_product_creation(self):
+        with self.app as c:
+            response = c.post('/api/v1/products')
+            self.assertEqual(response.status_code, 400)
+            resp = c.post('/api/v1/products', data=json.dumps(
+                {'name': 'pixel', 'category': 'electronic', 'price': 40}), content_type='application/json')
+            self.assertEqual(resp.status_code, 201)
