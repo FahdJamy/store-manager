79ef4bf318281d33fa96474aa1f53f4f267886f6
muhwezideo <fahdjamy2@gmail.com>
2018-10-16 15:09:08 +0300

    [Deliver #161216713] write api endpoint for create new sales record

---
+++ app/routes/sales.py
@@ -0,0 +1,25 @@
+from app import app, api
+from flask_restplus import Resource, fields
+from app.models.sales import Sales
+
+sale_model = api.model('Product', {
+		'name' : fields.String(description='product name', required=True, min_length=5),
+		'category' : fields.String(description='product category', required=True, min_length=5),
+		'price' : fields.Integer(description='Product price', required=True),
+		'quantity' : fields.Integer(description='Quantity', required=True)
+	})
+sales_model = Sales ()
+
+""" Create a sales record by sales attndant """
+@api.route('/sales')
+class Sales (Resource):
+	@api.expect(sale_model, validate=True)
+	def post(self):
+		product_data = api.payload
+		name = product_data['name']
+		category = product_data['category']
+		price = product_data['price']
+		quantity = product_data['quantity']
+		username = 'mags'
+		new_sales_record = sales_model.create_new_sale_record(name, price, category, quantity, username)
+		return {'Sales record' : new_sales_record}, 201
