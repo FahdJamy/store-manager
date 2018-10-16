411f3d0c96755ee374ef08671f2be6e2f2ef62fa
muhwezideo <fahdjamy2@gmail.com>
2018-10-16 12:59:46 +0300

    [Deliver #161214653] add route for create product endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from app.utils.config import Config
+from flask_restplus import Api
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+api = Api(app, prefix="/api/v1",version='1')
 
 from .routes import products
