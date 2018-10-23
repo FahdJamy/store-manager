a4c02b33061ddbe3278fd138f82781e030cc362c
muhwezideo <fahdjamy2@gmail.com>
2018-10-23 11:53:08 +0300

    [Chore #161409577] add tests to test for wrong input sales product category value that is not a 'string'

--- app/tests/test_validation.py
+++ app/tests/test_validation.py
@@ -25,7 +25,6 @@ class TestValidationCase(TestCase):
             message = str(data['errors']['category'])
             self.assertEqual(message, "5000 is not of type 'string'")
 
-
     def test_product_price_value_validation(self):
         with self.app as c:
             response = c.post('/api/v1/products', data=json.dumps(
@@ -35,11 +34,18 @@ class TestValidationCase(TestCase):
             message = str(data['errors']['price'])
             self.assertEqual(message, "'sixty' is not of type 'integer'")
 
-    def test_sales_product_price_value_validation(self):
+    def test_sales_product_name_and_category_value_validation(self):
         with self.app as c:
-        	response = c.post('/api/v1/sales', data=json.dumps(
+            response = c.post('/api/v1/sales', data=json.dumps(
                 {'name': 200, 'category': 'electronic', 'price': 40, 'quantity': 2}), content_type='application/json')
-        	self.assertEqual(response.status_code, 400)
-        	data = json.loads(response.data)
-        	message = str(data['errors']['name'])
-        	self.assertEqual(message, "200 is not of type 'string'")
+            self.assertEqual(response.status_code, 400)
+            data = json.loads(response.data)
+            message = str(data['errors']['name'])
+            self.assertEqual(message, "200 is not of type 'string'")
+
+            response = c.post('/api/v1/sales', data=json.dumps(
+                {'name': 'google pixel', 'category': 700.9, 'price': 40, 'quantity': 2}), content_type='application/json')
+            self.assertEqual(response.status_code, 400)
+            data = json.loads(response.data)
+            message = str(data['errors']['category'])
+            self.assertEqual(message, "700.9 is not of type 'string'")
