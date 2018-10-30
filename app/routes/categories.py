2c16020b7f534b76aa19aca0fe2b125397af14e0
muhwezideo <fahdjamy2@gmail.com>
2018-10-30 22:32:22 +0300

    [Deliver #161600237] create api endpoint for get all available categories

--- app/routes/categories.py
+++ app/routes/categories.py
@@ -8,10 +8,10 @@ category_info = api.model('Category', {
     'description': fields.String(description='category description', required=True, min_length=2)
 })
 Authorization = {'token_key': {
-            'in': 'header',
-            'type': 'JWT',
+    'in': 'header',
+    'type': 'JWT',
             'description': 'Token is required'
-        }}
+}}
 category_model = Category()
 
 
@@ -27,7 +27,14 @@ class Category (Resource):
         category_data = api.payload
         category_name = category_data['name'].strip().capitalize()
         description = category_data['description']
-        new_category = category_model.create_category(category_name, description)
+        new_category = category_model.create_category(
+            category_name, description)
         if new_category == 'category created successfully':
             return {'message': 'category has been successfully created !!!'}, 201
         return {'message': 'sorry category with name {} already exists'.format(category_name)}, 400
+
+    def get(self):
+        categories = category_model.get_all_available_categories()
+        if categories:
+            return {'categories': categories}, 200
+        return {'message': 'sorry, no categories exist in the database'}, 400
