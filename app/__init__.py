b637d03a16c1843ddd13064e77611de8b0401538
muhwezideo <fahdjamy2@gmail.com>
2018-10-31 10:46:04 +0300

    change switching config name

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,11 @@ from app.config import config_app
 
 app = Flask(__name__)
 
-config_name = os.getenv('PRODUCTION')
-if os.getenv('TESTING'):
+if os.getenv('PRODUCTION'):
+	config_name = os.getenv('PRODUCTION_ENV')
+elif os.getenv('TESTING'):
     config_name = os.getenv('TESTING_CONF')
-elif os.getenv('DEPLOY'):
+else:
     config_name = os.getenv('DEPLOYMENT')
 
 app.config.from_object(config_app[config_name])
