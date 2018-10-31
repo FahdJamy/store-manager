a0e760360a7db4826379ee9ec371be4164d66d58
muhwezideo <fahdjamy2@gmail.com>
2018-10-31 15:18:40 +0300

    modify travis file

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ if os.getenv('CONFIG_NAME') == 'production':
 	config_name = os.getenv('PRODUCTION_CONF')
 elif os.getenv('CONFIG_NAME') == 'testing':
 	config_name = os.getenv('TESTING_CONF')
-elif os.getenv('CONFIG_NAME') == 'deployment':
+else:
 	config_name = os.getenv('DEPLOYMENT_CONF')
 
 app.config.from_object(config_app[config_name])
