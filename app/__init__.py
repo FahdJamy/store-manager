101dbade310bb24d4cf383574856c7678e679468
muhwezideo <fahdjamy2@gmail.com>
2018-10-31 15:07:32 +0300

    modify environment variable to make app change config classes on exportation of CONFIG_NAME enviroment variable

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,13 @@ from flask_restplus import Api
 from app.config import config_app
 
 app = Flask(__name__)
-
-if os.getenv('PRODUCTION'):
-	config_name = os.getenv('PRODUCTION_ENV')
-elif os.getenv('TESTING'):
+config_name = ""
+if os.getenv('CONFIG_NAME') == 'production':
+	config_name = os.getenv('PRODUCTION_CONF')
+elif os.getenv('CONFIG_NAME') == 'testing':
 	config_name = os.getenv('TESTING_CONF')
-elif os.getenv('PRODUCTION'):
-	config_name = os.getenv('DEPLOYMENT')
+elif os.getenv('CONFIG_NAME') == 'deployment':
+	config_name = os.getenv('DEPLOYMENT_CONF')
 
 app.config.from_object(config_app[config_name])
 print(app.config['DATABASE'])
